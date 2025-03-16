8857daad84bd0c08b6c176bb67d17217f8c51d4d
AlphaFoxz <841958335@qq.com>
2025-03-16 22:39:45 +0800

    debug && docs

--- cli/scripts/build-cli.mjs
+++ cli/scripts/build-cli.mjs
@@ -56,6 +56,9 @@ function esBuild() {
     plugins: [],
     target: 'node18',
     tsconfig: path.join(__dirname, '..', 'tsconfig.build-cli.json'),
+    banner: {
+      js: '#!/bin/bash',
+    },
   })
 }
 
