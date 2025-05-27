0306d6251d448e88890fafd5c1c86f600d129c4a
AlphaFoxz <841958335@qq.com>
2025-05-27 11:26:40 +0800

    v0.1.0-beta.13

--- cli/src/utils/package-info.ts
+++ cli/src/utils/package-info.ts
@@ -1,6 +1,6 @@
 export default {
   "name": "@ddd-tool/domain-designer-cli",
-  "version": "0.1.0-beta.12",
+  "version": "0.1.0-beta.13",
   "private": true,
   "type": "module",
   "files": [
@@ -15,5 +15,5 @@ export default {
   },
   "readme": "ERROR: No README data found!",
   "homepage": "https://github.com/ddd-tool/domain-designer-cli-node#readme",
-  "_id": "@ddd-tool/domain-designer-cli@0.1.0-beta.12"
+  "_id": "@ddd-tool/domain-designer-cli@0.1.0-beta.13"
 }
